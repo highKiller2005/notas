ccbb03fee27e3fbdada669ea2f68181fbb5cc26f
THIAGO LUCIANO DA SILVEIRA <thiago_l_silveira@estudante.sesisenai.org.br>
2024-09-30 08:27:35 -0300

    batman

---
+++ db.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS `thiago_kaua`;
+CREATE DATABASE `thiago_kaua`;
+USE `thiago_kaua`;
+
+CREATE TABLE `notas` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `titulo` VARCHAR(100) NOT NULL,
+    `conteudo` TEXT,
+    `categoria` VARCHAR(100) NOT NULL,
+    `criado_em` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `user` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `nome` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) UNIQUE NOT NULL,
+    `senha` VARCHAR(255) NOT NULL
+);
